a1ef28644f6057b325f7b470033d2306b59e7ae6
Maksim <auksinistonic@gmail.com>
2022-09-05 02:21:48 +0300

    feat: snake grows when eats

--- script/snake.js
+++ script/snake.js
@@ -1,6 +1,13 @@
 const canvas = document.getElementById('main-canvas')
 const ctx = canvas.getContext('2d')
 
+class SnakePart {
+  constructor(x, y) {
+    this.x = x
+    this.y = y
+  }
+}
+
 const SPEED = 14
 const TILE_COUNT = 20
 const TILE_SIZE = 20
@@ -10,6 +17,8 @@ let AXIS_X = 0
 let AXIS_Y = 0
 let FOOD_X = canvas.width / 4
 let FOOD_Y = canvas.width / 4
+const SNAKE_PARTS = []
+let tailLength = 2
 
 function drawGameBoard() {
   const FIRST_BOARD_COLOR = '#2c2c54'
@@ -45,10 +54,23 @@ function drawGame() {
 drawGame()
 
 function drawSnake() {
+  ctx.fillStyle = '#90ee90'
+  for (let i = 0; i < SNAKE_PARTS.length; i++) {
+    let part = SNAKE_PARTS[i]
+
+    ctx.fillRect(part.x, part.y, TILE_SIZE, TILE_SIZE)
+    console.log(SNAKE_PARTS)
+  }
+  SNAKE_PARTS.push(new SnakePart(HEAD_X, HEAD_Y))
+  if (SNAKE_PARTS.length > tailLength) {
+    SNAKE_PARTS.shift()
+  }
+
   ctx.fillStyle = 'green'
   ctx.fillRect(HEAD_X, HEAD_Y, TILE_SIZE, TILE_SIZE)
 }
 
+// console.log(SNAKE_PARTS[0])
 function drawFood() {
   ctx.fillStyle = 'red'
   ctx.fillRect(FOOD_X, FOOD_Y, TILE_SIZE, TILE_SIZE)
@@ -62,6 +84,7 @@ function didSnakeAte() {
     FOOD_Y = random - (random % TILE_SIZE)
     console.log(FOOD_Y)
     console.log(HEAD_X, HEAD_Y)
+    tailLength++
   }
 }
 
