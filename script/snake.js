3ba4b52b24f11a8d73fb3967c5b4e40ce4b09d5f
Maksim <auksinistonic@gmail.com>
2022-09-05 00:19:13 +0300

    feat: game board styled

--- script/snake.js
+++ script/snake.js
@@ -1,2 +1,27 @@
 const canvas = document.getElementById('main-canvas')
 const ctx = canvas.getContext('2d')
+
+const TILE_COUNT = 20
+
+function drawGameBoard() {
+  const FIRST_BOARD_COLOR = '#2c2c54'
+  const SECOND_BOARD_COLOR = '#474787'
+
+  for (let i = 1; i <= 40; i++) {
+    for (let q = 1; q <= 40; q++) {
+      if ((i + q) % 2 == 0) {
+        ctx.fillStyle = FIRST_BOARD_COLOR
+      } else {
+        ctx.fillStyle = SECOND_BOARD_COLOR
+      }
+      ctx.fillRect(
+        TILE_COUNT * (i - 1),
+        TILE_COUNT * (q - 1),
+        TILE_COUNT,
+        TILE_COUNT,
+      )
+    }
+  }
+}
+
+drawGameBoard()
